298fc59a063a1d4ddfc76807c738f228bdfe9eaa
aishvarya779 <aishvaryatiwari123@gmail.com>
2025-06-21 23:40:04 +0530

    gh pages changes reverted

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,16 +13,4 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {
-  reactStrictMode: false,
-  trailingSlash: true,
-  assetPrefix: isProd ? '/aishvarya779.github.io/' : '',
-  basePath: isProd ? '/aishvarya779.github.io' : '',
-  output: 'export',
-};
-
-module.exports = nextConfig
-
 export default eslintConfig;
